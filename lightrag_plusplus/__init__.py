cf97d8354582acea643171e3240bbacbdabf35e2
Marc Hansen <hansen.marc@gmail.com>
2026-02-13 05:01:04 +0000

    fix: extract version to standalone _version.py for CI build
    Solves ModuleNotFoundError in isolated CI environments by removing
    dynamic version import that depends on local lightrag package.

--- lightrag_plusplus/__init__.py
+++ lightrag_plusplus/__init__.py
@@ -16,13 +16,8 @@ _lightrag_dir = os.path.join(_lightrag_plusplus_dir, "lightrag")
 if _lightrag_dir not in sys.path:
     sys.path.insert(0, _lightrag_dir)
 
-from lightrag import (
-    LightRAG,
-    QueryParam,
-    __version__,
-    __author__,
-    __url__,
-)
+from lightrag import LightRAG, QueryParam
+from lightrag_plusplus._version import __version__, __author__, __url__
 
 __all__ = [
     "LightRAG",
